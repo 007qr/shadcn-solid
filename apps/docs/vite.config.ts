2f316c947b74631a13acec90e9b9c56d0f00100e
007qr <aayushpatil558321@gmail.com>
2026-04-10 12:06:37 +0530

    chore: switch to cloudflare-pages target

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,9 +1,9 @@
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite"
+import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import viteSolid from "vite-plugin-solid"
 import tsConfigPaths from "vite-tsconfig-paths"
-
 import content from "./plugins/content"
 import mdx from "./plugins/mdx"
 
@@ -12,26 +12,18 @@ export default defineConfig({
     port: 3001,
   },
   plugins: [
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     mdx(),
     content(),
     tanstackStart({
       customViteSolidPlugin: true,
-      target: "netlify",
-      sitemap: {
-        enabled: true,
-      },
-      prerender: {
-        crawlLinks: true,
-      },
+      target: 'cloudflare-pages',
+      sitemap: { enabled: true },
+      prerender: { crawlLinks: true },
     }),
-    viteSolid({
-      ssr: true,
-      extensions: [".mdx"],
-    }),
+    viteSolid({ ssr: true, extensions: [".mdx"] }),
   ],
   resolve: {
     noExternal: ["@kobalte/core", "cmdk-solid"],
